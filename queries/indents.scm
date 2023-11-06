f26273f0ef6edffda59f96c96f8410f3ac36dfe7
al <alessandro.schena.97@protonmail.com>
2023-11-06 10:32:42 -0500

    file-tree fixup

---
+++ queries/indents.scm
@@ -0,0 +1,13 @@
+[
+  (notes)
+  (feature_declaration)
+] @indent
+
+[
+  (feature_clause)
+  (internal) 
+] @indent @extend
+
+[
+  "end"
+] @outdent.always @extend.prevent_once
---
+++ queries/textobjects.scm
@@ -0,0 +1,9 @@
+(comment)+ @comment.around
+(comment) @comment.inside
+(formal_arguments) @parameter.around
+(entity_declaration_list) @parameter.inside
+(attribute_or_routine) @function.around
+(compound) @function.inside
+(class_declaration) @class.around
+(features) @class.inside
+
