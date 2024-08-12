439e2464c0d7fe6d30ef79de37b9a3aab302a21c
Alessandro Schena <Alessandro.Schena@constructor.org>
2024-08-12 18:03:40 +0200

    feat: add neovim textobject queries.

--- queries/textobjects.scm
+++ queries/textobjects.scm
@@ -1,15 +1,15 @@
 [
   (comment)+
   (header_comment)+
-] @comment.around
+] @comment.outer
 [
   (comment)
   (header_comment)
-] @comment.inside
-(formal_arguments) @parameter.around
-(entity_declaration_group) @parameter.inside
-(attribute_or_routine) @function.around
-(feature_body) @function.inside
-(class_declaration) @class.around
-(feature_clause) @class.inside
+] @comment.inner
+(formal_arguments) @parameter.outer
+(entity_declaration_group) @parameter.inner
+(attribute_or_routine) @function.outer
+(feature_body) @function.inner
+(class_declaration) @class.outer
+(feature_clause) @class.inner
 
