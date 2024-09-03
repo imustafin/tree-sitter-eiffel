8ca406a8ede9a2407b28394bd945e3bb72f88c76
Ilgiz Mustafin <ilgimustafin@gmail.com>
2024-09-03 12:20:51 +0200

    feat: generate files with new tree-sitter-cli

--- bindings/go/binding_test.go
+++ bindings/go/binding_test.go
@@ -3,8 +3,8 @@ package tree_sitter_eiffel_test
 import (
 	"testing"
 
-	tree_sitter "github.com/smacker/go-tree-sitter"
-	"github.com/tree-sitter/tree-sitter-eiffel"
+	tree_sitter "github.com/tree-sitter/go-tree-sitter"
+	tree_sitter_eiffel "github.com/tree-sitter/tree-sitter-eiffel/bindings/go"
 )
 
 func TestCanLoadGrammar(t *testing.T) {
